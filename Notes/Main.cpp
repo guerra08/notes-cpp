c1e8157a11389f1dcce069a29504de796d65eedb
Bruno Guerra <bruno.guerra@edu.pucrs.br>
2020-12-06 01:50:28 -0300

    feat
    Menu bar; reads file from txt store.

--- Notes/Main.cpp
+++ Notes/Main.cpp
@@ -6,9 +6,20 @@ wxEND_EVENT_TABLE()
 
 Main::Main() : wxFrame(nullptr, wxID_ANY, "Notes", wxPoint(0, 0), wxSize(800, 600))
 {
+	menu_bar = new wxMenuBar();
+	menu = new wxMenu();
+	config_item = new wxMenuItem(menu, wxID_ANY, _("Storage file"), _("Toggle the management area"), wxITEM_NORMAL);
+
+	menu->Append(config_item);
+	menu_bar->Append(menu, _T("&File"));
+
+	SetMenuBar(menu_bar);
+
 	input = new wxTextCtrl(this, wxID_ANY, "", wxPoint(24, 40), wxSize(240, 20));
 	add_button = new wxButton(this, 201, "Add", wxPoint(24, 64), wxSize(40, 16));
 	data_list = new wxListBox(this, wxID_ANY, wxPoint(24, 120), wxSize(280, 280), 0, NULL, wxLB_MULTIPLE);
+
+	PopulateList();
 
 	data_list->Connect(wxEVT_RIGHT_DOWN, wxMouseEventHandler(Main::OnRightPressOnList), NULL, this);
 }
@@ -50,4 +61,21 @@ void Main::OnMenuPress(wxCommandEvent& event)
 		data_list->Delete(i);
 	}
 	event.Skip();
+}
+
+/*
+	Populates the list with the notes on %userprofile%\documents
+*/
+void Main::PopulateList() {
+	wxString docs_path = wxStandardPaths::Get().GetDocumentsDir();
+	wxFileName fname = wxFileName(docs_path, "notes-store.txt");
+	if (fname.FileExists()) {
+		wxFileInputStream input(fname.GetFullPath());
+		wxTextInputStream text(input);
+		while (input.IsOk() && !input.Eof())
+		{
+			wxString line = text.ReadLine();
+			data_list->AppendString(line);
+		}
+	}
 }
--- Notes/Main.h
+++ Notes/Main.h
@@ -2,6 +2,10 @@
 
 #include "wx/wx.h"
 #include <wx/listctrl.h>
+#include <wx/wfstream.h>
+#include <wx/txtstrm.h>
+#include <wx/stdpaths.h>
+#include <wx/filename.h>
 
 class Main : public wxFrame
 {
@@ -14,11 +18,17 @@ public:
 	wxButton* add_button = nullptr;
 	wxTextCtrl* input = nullptr;
 	wxListBox* data_list = nullptr;
+	wxMenuBar* menu_bar = nullptr;
+	wxMenu* menu = nullptr;
+	wxMenuItem* config_item = nullptr;
 
 	void OnButtonPress(wxCommandEvent& evt);
 	void OnRightPressOnList(wxMouseEvent& evt);
 	void OnMenuPress(wxCommandEvent& evt);
 
 	wxDECLARE_EVENT_TABLE();
+
+private:
+	void PopulateList();
 };
 
